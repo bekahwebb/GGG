b2e09bc888caae065fde27008d429ca3b59b791b
Rebekah Webb <bekahwebb@gmail.com>
2024-11-06 10:18:59 -0700

    naive bayes not so naive let's go with a score of.75047 already above the cutoff!

--- GhostsGhoulsGoblins.R
+++ GhostsGhoulsGoblins.R
@@ -22,10 +22,12 @@ library(vroom)
 library(embed)
 library(themis)
 library(parsnip)
+library(discrim)
+library('naivebayes')
 
 ggg_test <- read_csv('test.csv')
 ggg_train <- read_csv('train.csv')
-ggg_trainNA <- read_csv('trainWithMissingValues.csv') 
+#ggg_trainNA <- read_csv('trainWithMissingValues.csv') 
 # ggg_train
 # ggg_trainNA
 
@@ -61,7 +63,7 @@ ggg_recipe <- recipe(type~., data=ggg_train) %>%
 
 rf_ggg_model <- rand_forest(mtry = tune(),
                         min_n=tune(),
-                        trees=500) %>%
+                        trees=999) %>%
   set_engine("ranger") %>%
   set_mode("classification")
 
@@ -104,9 +106,73 @@ rf_ggg_predictions <- final_wf %>%
 rf_ggg_kaggle_submission <- rf_ggg_predictions%>%
   rename(type=.pred_class) %>%
   bind_cols(., ggg_test) %>% #Bind predictions with test data
-  select(id, type)  #keep Id, ACTION for submission
+  select(id, type)  #keep Id, type for submission
 
 
 ## Write out the file
 vroom_write(x=rf_ggg_kaggle_submission, file="rfGGGPreds.csv", delim=",")
-#public score of .71455
+#public score of .71455 with 500 trees
+#try 900 trees, improved to .72022
+#try 1100 trees, went back down to .71455
+# try 999 trees, public score .71833
+#.729 svm score increased some of the tuning paramaters .732
+#.681 knn
+#.747 is the cutoff
+#.750 let's go! with naive bayes, improved from .72022 with making id as a feature
+
+
+#my recipe
+# Feature Engineering naive bayes
+ggg_recipe <- recipe(type~., data = ggg_trainNA) %>% 
+  step_mutate(color = as.factor(color)) %>% 
+  step_mutate(id, features = id) %>%
+  step_range(all_numeric_predictors(), min = 0, max = 1)
+
+
+ggg_model <- naive_Bayes(Laplace=tune(), smoothness=tune()) %>%
+  set_mode("classification") %>%
+  set_engine("naivebayes") # install discrim library for the naivebayes
+
+## Create a workflow with model & recipe
+
+ggg_wf <- workflow() %>%
+  add_recipe(ggg_recipe) %>%
+  add_model(ggg_model)
+
+## Set up grid of tuning values
+tuning_grid <- grid_regular(Laplace(), smoothness(),levels = 3)
+
+## Set up K-fold CV
+folds <- vfold_cv(ggg_train, v = 10, repeats=1)
+
+## Run the CV
+CV_results <- ggg_wf %>%
+  tune_grid(resamples=folds,
+            grid=tuning_grid,
+            metrics=metric_set(roc_auc)) 
+
+## Find best tuning parameters
+bestTune <- CV_results %>%
+  select_best(metric="roc_auc")
+
+## Finalize workflow and predict
+final_wf <-
+  ggg_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data=ggg_train)
+
+## Predict
+ggg_predictions <- final_wf %>%
+  predict(new_data =ggg_test, type="class")
+
+
+## Format the Predictions for Submission to Kaggle
+naivebayes_ggg_kaggle_submission <- ggg_predictions%>%
+  rename(type=.pred_class) %>%
+  bind_cols(., ggg_test) %>% #Bind predictions with test data
+  select(id, type)  #keep Id, type for submission
+
+
+## Write out the file
+vroom_write(x=naivebayes_ggg_kaggle_submission, file="nbGGGPreds.csv", delim=",")
+#public score 0.75047, done, the cutoff is .747
